6a5e0bf8f4eef5bb0c164401cc3e468b9e48c891
M0nk <ea.chukhontsev@icloud.com>
2024-06-05 13:29:33 +0300

    Lesson-4-5.HomeWork_3_4_5

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,72 @@ public class Main {
         System.out.println ("isEqual:  "+ Utils.isEqual (13,13, 13));
         System.out.println ("lastNUm:  "+ Utils.lastNumSum (Utils.lastNumSum(Utils.lastNumSum(Utils.lastNumSum(5,11),123),14),1));
 
-    }
+        //        -----------------------------------------ЗАДАНИЕ 3 -----------------------------------------
+        System.out.println("MOD: " + Utils.safeDiv(99, 9));
+        System.out.println("ABS: " + Utils.abs(5));
+        System.out.println("MAX: " + Utils.max(53, 1));
+        System.out.println("makeDecision: " + Utils.makeDecision(13, 13));
+        System.out.println("MAX3: " + Utils.max3(12, 13, -15));
+        System.out.println("SUM3: " + Utils.sum3(14, 5, -8));
+        System.out.println("SUM2: " + Utils.sum2(14, -3));
+        System.out.println("IS35: " + Utils.is35(15));
+        System.out.println("MAGIC6: " + Utils.magic6( 6,  10));
+        System.out.println("AGE: " + Utils.age( 44));
+        System.out.println("DAY: " + Utils.day( 5));
+        System.out.println("\nWEEK: " );
+        Utils.printDays("Среда");
+//        -----------------------------------------ЗАДАНИЕ 4 -----------------------------------------
+        Utils.guessGame();
+        System.out.println(Utils.listNums(5));
+        System.out.println(Utils.reverseListNums(5));
+        System.out.println(Utils.chet(12));
+        System.out.println(Utils.pow(2,5));
+        System.out.println(Utils.numLen(0));
+        System.out.println(Utils.equalNum(141));
+        Utils.square(6);
+        Utils.leftTriangle(3);
+        Utils.rightTriangle(5);
+//        -----------------------------------------ЗАДАНИЕ 5 -----------------------------------------
 
+        int [] arr={1,2,-3,0,-12,44,-77};
+         arr= new int[]{1, 5, 7, 4, 7, 5, 1};
+        int [] arr1= {1,2,3};
+        int [] arr2= {7,8,9};
+        System.out.println(Utils.findFirst(arr, 5));
+        System.out.println(Utils.findLast(arr, 1));
+        System.out.println(Utils.maxAbs(arr));
+        System.out.println(Utils.countPositive(arr));
+        System.out.println(Utils.palindrom(arr));
+        int [] arrx= Utils.reverseBack(arr);
+        for (int x: arrx) {System.out.print(x+" ");}
+          int [] arr3= Utils.concat(arr1,arr2);
+          for(int i:arr3){
+                System.out.print(i+" ");
+          }
+         arr= new int[]{1, 2, 3, 8, 2, 2, 9};
+        int [] reslt = Utils.findAll(arr, 2);
+        for (int x:reslt){
+            System.out.print(x+" ");
+        }
+        arr= new int[]{1, 2, -3, 4, -2, 2, -5};
+        reslt = Utils.deleteNegative(arr);
+        for (int x:reslt){
+            System.out.print(x+" ");
+        }
+        Utils.reverse(arr);
+        arr= new int[]{1, 2, 3, 4, 5};
+        int [] ins= {7,8,9};
+        int x=99;
+        int pos=5;
+        int [] rslt = Utils.add(arr, x,pos);
+        int [] rst = Utils.arradd(arr, ins, pos);
+       for (int z:rslt){System.out.print(z+" ");}
+       System.out.println();
+        for (int y:rst){System.out.print(y+" ");}
+
+        String s = "1+2";
+        String[] spl = s.split("\\+");
+        for (String str:spl){System.out.println(str);}
+    }
 
 }
--- src/Utils.java
+++ src/Utils.java
@@ -38,4 +38,382 @@ public class Utils {
         return (a % 10)+(b % 10);
     }
 
+    public static int safeDiv(int x, int y) {
+        return x != 0 && y != 0 ? x / y : 0;
+    }
+
+    public static int abs(int x) {
+        return x >= 0 ? x : -x;
+    }
+
+    public static int max(int x, int y) {
+        return x > y ? x : y;
+    }
+
+    public static String makeDecision(int x, int y) {
+        if (x == y) {
+            return "" + x + "=" + y;
+        } else {
+            return x > y ? "" + x + ">" + y : "" + x + "<" + y;
+        }
+    }
+
+    public static int max3(int x, int y, int z) {
+        if (x > y && x > z) {
+            return x;
+        } else {
+            return x < y && y > z ? y : z;
+        }
+    }
+
+    public static boolean sum3(int x, int y, int z) {
+        return x + y == z || x == y + z || z + x == y;
+    }
+
+    public static int sum2(int x, int y) {
+        return x + y > 10 & x + y < 20 ? 20 : x + y;
+    }
+
+    public static boolean is35(int x) {
+        if (0 == x % 3 & 0 == x % 5) {
+            return false;
+        } else {
+            return 0 == x % 3 || 0 == x % 5;
+        }
+    }
+
+    public static boolean magic6(int x, int y) {
+        return x == 6 || y == 6 || 6 == x - y || y - x == 6 || 6 == x + y;
+    }
+
+    public static String age(int x) {
+        if (x % 10 == 1 & x != 11) {
+            return "" + x + " год";
+        } else {
+            return 2 == x % 10 || 3 == x % 10 || 4 == x % 10 && 12 != x && 13 != x && 14 != x ? "" + x + " года" : "" + x + " лет";
+        }
+    }
+
+    public static String day(int x) {
+        switch (x) {
+            case 1:
+                return "Понедельник";
+            case 2:
+                return "Вторник";
+            case 3:
+                return "Среда";
+            case 4:
+                return "Четверг";
+            case 5:
+                return "Пятница";
+            case 6:
+                return "Суббота";
+            case 7:
+                return "Воскресенье";
+            default:
+                return "Ошибка!";
+        }
+    }
+
+    public static void printDays(String x) {
+        switch (x) {
+            case "Понедельник":
+                System.out.println("Понедельник");
+            case "Вторник":
+                System.out.println("Вторник");
+            case "Среда":
+                System.out.println("Среда");
+            case "Четверг":
+                System.out.println("Четверг");
+            case "Пятница":
+                System.out.println("Пятница");
+            case "Суббота":
+                System.out.println("Суббота");
+            case "Воскресенье":
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("это не день недели!");
+        }
+    }
+    public static String listNums(int x) {
+        String result ="";
+        for (int i = 0; i < x; i++) {
+            result+=i+" ";
+        }
+        return result;
+    }
+    public static String reverseListNums(int x){
+        String result ="";
+        for (;x>0;x--){
+            result+=x+" ";
+        } return result;
+    }
+    public static String chet(int x) {
+        String result ="";
+        for (int i = 0; i <=x; i+=2) {
+            result += " " + i;
+
+        }
+
+        return result;
+    }
+    public static int pow(int x, int y){
+        if (x==0) return 0;
+        else if (y==0) return 1;
+        else {
+            int z =1;
+            for (;y!=0;y--){
+                z=z*x;}
+            return z;
+        }
+    }
+    public static int numLen(long x) {
+        if (x==0) return 1;
+        int z =0;
+        for (;x!=0; z++)
+        {
+            x=x/10;
+        } return z;
+    }
+    public static int equalNum(int x)
+    {
+        return x/10;
+
+    }
+    public static void square(int x) {
+        for (int i=x; i != 0; i--) {
+            for (int y = x; y != 0; y--) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void leftTriangle(int x){
+        for ( ;x!=0;x--){
+            for (int i=0;i!=x;i++){
+                System.out.print('*');
+            }System.out.println();
+        }
+    }
+    public static void rightTriangle (int x){
+        for (int i=0; i!=x; i++) {
+            for (int j=0;j<(x-i-1);j++){
+                System.out.print(' ');
+            }
+            for (int z=0; z<i; z++){
+                System.out.print('*');
+            }System.out.println();
+        }
+
+
+    }
+
+
+
+    public static void guessGame() {
+        int randomNum = 3;
+        int trysum = 0;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("What number am I thinking (0 to 9)? :");
+        int x = sc.nextInt();
+        while (x != randomNum){
+            System.out.println("No, try again");
+            trysum++;
+            x = sc.nextInt();
+        }
+
+        System.out.println("Yes, it`s " + randomNum);
+        System.out.println("you tried " + (trysum+1) + " times");
+
+    }
+    public static int findFirst(int[] arr, int x) {
+        int i=0;
+        for (int n:arr){
+            if (x == n) return i;
+            i++;
+        }
+        return -1;
+    }
+    public static int findLast(int[] arr, int x){
+
+        for (int i = arr.length-1; i>0; i--){
+            if (arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int maxAbs(int[] arr){
+        int max =0;
+        boolean sing = false;
+        for (int x:arr){
+            if (x<0) {
+                x*=-1;
+                if ( x > max) {
+                    sing = true;
+                    max = x;
+                }
+            }
+            if (x>max){
+                sing = false;
+                max = x;
+            }
+
+        }
+
+
+        if (sing == true) {
+            return max*=-1;
+        } return max;
+    }
+    public static int countPositive(int[] arr){
+        int i =0;
+        for(int x:arr) {
+            if (x>0)
+            {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static boolean palindrom(int[] arr){
+        boolean flag = true;
+        int z = arr.length;
+        for (int i = 0; i<z/2 && z!=0;i++){
+            if (arr[i]!=arr[z-i-1])
+            {flag=false; break;}
+        }
+        return flag;
+
+    }
+
+    public static void reverse(int[] arr){
+
+        for(int i:arr){
+            System.out.print(i+" ");
+        } System.out.println();
+        int change =0;
+        for(int i = 0; i<(arr.length-1)/2; i++){
+            change =arr[i];
+            arr[i]= arr[arr.length-i-1];
+            arr[arr.length-i-1] = change;
+        }
+        for(int i:arr){
+            System.out.print(i+" ");
+        }System.out.println();
+    }
+    public static int[] reverseBack(int[] arr) {
+        int [] reversarr = new int[arr.length];
+        int index=0;
+        for (int i = arr.length - 1; i>0 ; i--, index++) {
+            reversarr[index] = arr[i];
+        }
+        return reversarr;
+    }
+    public static int[] concat(int[] arr1, int[] arr2){
+
+        int [] result = new int[arr1.length+arr2.length];
+        int i = 0;
+        for (int x:arr1){
+            result[i]= x;
+            i++;
+        }
+        for (int x:arr2){
+            result[i]=x;
+            i++;
+        }
+        return result;
+    }
+    public static int[] findAll(int[] arr, int x) {
+        int i =0;
+        for (int z: arr){if (z==x){i++;}}
+        int [] result = new int [i];
+        i=0;
+        int n=0;
+        for (int y:arr){if (y==x){result[n]=i; n++;}i++;}
+        return result;
+    }
+
+    public static int[] deleteNegative(int[] arr){
+
+        int i =0;
+        for (int x:arr){
+            if (x>0){
+                i++;
+            }
+        }
+        int [] result = new int[i];
+        i=0;
+        for (int z: arr){
+            if(z>0){
+                result[i] = z;
+                i++;
+            }
+        } return result;
+    }
+
+    public static int[] add(int[] arr, int x, int pos){
+        int [] result = new int[arr.length+1];
+        int [] stack = new int [arr.length-pos];
+        int z =0;
+        if (pos == arr.length){result[arr.length]=x;}
+        for ( int i = pos; i<arr.length; i++)
+        {
+            stack[z] = arr[i];
+            z++;
+        }
+        for (int i=0; i<arr.length;i++){
+            if (i==pos){
+                result[i] =x;
+                i++;
+                for (int y:stack){ result[i]=y;i++;}
+                break;
+            }
+            result[i] = arr[i];
+
+        }
+
+
+        return result;
+
+
+    }
+    public static int[] arradd(int[] arr, int[] ins, int pos) {
+        int[] result = new int[arr.length + ins.length];
+        int i = 0;
+        if (pos == arr.length){
+            for (int x : arr){
+                result[i] = x;
+                i++;
+            }
+            for (int x:ins){
+                result[i]=x;
+                i++;
+            }
+        } else {
+            int [] stack = new int[arr.length-pos];
+            for (int x=pos; x<arr.length; x++){
+                stack[i]=arr[x];
+                i++;
+            }
+            i=0;
+            for (int x=0; x!=pos; x++){
+                result[i] = arr[x];
+                i++;
+            }
+            for (int x:ins){result[i]=x; i++;}
+            for (int x:stack){result[i]=x; i++;}
+
+        }
+
+
+
+
+        return result;
+    }
 }
+
+
