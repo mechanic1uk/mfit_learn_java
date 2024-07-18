a049ac94d38356116439f34312ab54829b4fe02c
M0nk <ea.chukhontsev@icloud.com>
2024-07-18 11:00:53 +0300

    1.4.7 update

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         Student vasya = new Student("Вася",3, 4, 5);
-        Student petya = new Student("Петя",  vasya.getAssessment());
-        Student andrew = new Student("Андрей",  vasya.getAssessment());
+        Student maxim = new Student("Максим");
+
         System.out.println(vasya);
-        System.out.println(petya);
-        System.out.println(andrew);
-        vasya.setAssessment(5);
-        petya.setAssessment(2,3);
-        petya.changeAssessment(2,5);
-        vasya.changeAllAssessment(5,5,5,3,2,2);
-        System.out.println(vasya);
-        System.out.println(petya);
-        System.out.println(andrew);
+        System.out.println(maxim);
+
+
 
     }
 
