23dd228b0a3d8c81c4d7f284d23823e4db90931c
M0nk <ea.chukhontsev@icloud.com>
2024-07-18 09:29:46 +0300

    1.4.2 update

--- src/Line.java
+++ src/Line.java
@@ -4,24 +4,28 @@ public class Line {
     private Point endPoint;
 
     Line (Point startPoint, Point endPoint){
-        this.startPoint = startPoint;
-        this.endPoint = endPoint;
+        this.startPoint = new Point(startPoint.getX(),startPoint.getY());
+        this.endPoint = new Point(endPoint.getX(), endPoint.getY());
+    }
+    Line (int startX, int startY, int endX, int endY){
+        this.startPoint = new Point(startX, startY);
+        this.endPoint = new Point(endX, endY);
     }
 
     public void setStartPoint(Point startPoint) {
-        this.startPoint = startPoint;
+        this.startPoint = new Point(startPoint.getX(),startPoint.getY());
     }
 
     public void setEndPoint(Point endPoint) {
-        this.endPoint = endPoint;
+        this.endPoint = new Point(endPoint.getX(), endPoint.getY());
     }
 
     public Point getStartPoint() {
-        return this.startPoint;
+        return new Point(this.startPoint.getX(), this.startPoint.getY());
     }
 
     public Point getEndPoint() {
-        return this.endPoint;
+        return new Point(this.endPoint.getX(), this.endPoint.getY());
     }
 
     public int distance (){
--- src/Main.java
+++ src/Main.java
@@ -10,40 +10,13 @@ public class Main {
         Line l1 = new Line(p1,p2);
         Line l2 = new Line(p3,p4);
         Line l3 = new Line(l1.getStartPoint(), l2.getEndPoint());
-        System.out.println(l1);
-        System.out.println(l2);
-        System.out.println(l3);
-
-        System.out.println();
-
-//
-          p2.setXY(11,23);
-          p1.setXY(999,777);
-          p3.setXY(88,27);
-          p4.setXY(66,44);
+        Line l4 = new Line(1,33,22,13);
 
 
         System.out.println(l1);
         System.out.println(l2);
         System.out.println(l3);
-
-        System.out.println();
-
-        p1 = new Point (33,23);
-        p2 = new Point (22,99);
-        p3 = new Point (66,77);
-        p4 = new Point (44,34);
-        l1.setStartPoint(p1);
-        l2.setStartPoint(p3);
-        l1.setEndPoint(p2);
-        l2.setEndPoint(p4);
-
-        System.out.println(l1);
-        System.out.println(l2);
-        System.out.println(l3);
-
-
-
+        System.out.println(l4);
 
 
 
