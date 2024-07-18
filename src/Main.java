7be0e6eff99fc72c9848bcf2d233e79b500400ea
M0nk <ea.chukhontsev@icloud.com>
2024-07-18 09:55:27 +0300

    1.6.6 update

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,29 @@ public class Main {
 
     //1.2.1
     Line l1 = new Line(1,1,10,15);
-    l1.distance();
+    Line l2 = new Line(4,5,7,9);
+    Line l3 = new Line(10,21,9,65);
+    Line l4 = new Line(l1.getStartPoint(), l2.getEndPoint());
+
+
 
 
         System.out.println(l1);
-        System.out.format("Расстояние между точками составляет: %.2f", l1.distance());
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        l1.setStartPoint(new Point(999,333));
+        l2.setStartPoint(new Point(99,33));
+        l3.setStartPoint(new Point(9,100));
+        l2.setEndPoint(new Point(9,100));
+        Point p1 = l3.getStartPoint();
 
 
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
 
     }
 
