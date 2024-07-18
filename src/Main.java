74034c5524cab7c33bf1b6d2dde34b78de918471
M0nk <ea.chukhontsev@icloud.com>
2024-07-18 10:58:34 +0300

    1.3.1 update

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-    //1.2.1
-    Line l1 = new Line(1,1,10,15);
-    Line l2 = new Line(4,5,7,9);
-    Line l3 = new Line(10,21,9,65);
-    Line l4 = new Line(l1.getStartPoint(), l2.getEndPoint());
-
-
-
-
-        System.out.println(l1);
-        System.out.println(l2);
-        System.out.println(l3);
-        System.out.println(l4);
-
-        l1.setStartPoint(new Point(999,333));
-        l2.setStartPoint(new Point(99,33));
-        l3.setStartPoint(new Point(9,100));
-        l2.setEndPoint(new Point(9,100));
-        Point p1 = l3.getStartPoint();
-
-
-        System.out.println(l1);
-        System.out.println(l2);
-        System.out.println(l3);
-        System.out.println(l4);
+        Student vasya = new Student("Вася",3, 4, 5);
+        Student petya = new Student("Петя",  vasya.getAssessment());
+        Student andrew = new Student("Андрей",  vasya.getAssessment());
+        System.out.println(vasya);
+        System.out.println(petya);
+        System.out.println(andrew);
+        vasya.setAssessment(5);
+        petya.setAssessment(2,3);
+        petya.changeAssessment(2,5);
+        vasya.changeAllAssessment(5,5,5,3,2,2);
+        System.out.println(vasya);
+        System.out.println(petya);
+        System.out.println(andrew);
 
     }
 
--- src/Student.java
+++ src/Student.java
@@ -2,70 +2,109 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-    private Name fullName;
+    private String name;
     private List <Integer> assessment = new ArrayList<>();
-    Student(String fullName, int ... assessment){
-        String [] tmp = fullName.split(" ");
-        if (tmp.length == 1){
-            this.fullName = new Name(tmp[0],null,null);
-        } else if (tmp.length == 2) {
-            this.fullName = new Name(tmp[0],tmp[1],null);
-
-        } else {
-            this.fullName = new Name(tmp[0],tmp[1],tmp[2]);
+    public Student(String name, int ...assessments){
+        this.name = name;
+        for (int i: assessments) {
+                chekAssessment(i);
+                this.assessment.add(i);
         }
-        if (assessment.length !=0 ) System.out.println("in");
-        for (int x: assessment) {
-            this.assessment.add(x);
-        }
-
-
+    }
+    public Student(String name, List<Integer> assessments){
+        this(name);
+        this.assessment.addAll(assessments);
     }
 
 
-
-    public String getAssessment() {
-        StringBuilder result = new StringBuilder();
-        for (int res =0; res < this.assessment.size(); res++){
-            if (res == this.assessment.size()-1) {
-                result.append(this.assessment.get(res));
-            }
-            else  result.append(this.assessment.get(res)).append(", ");
-
-        }
-        return result.toString();
-    }
-    public List<Integer> getAssassments(){
-        return this.assessment;
+    public boolean chekAssessment (int assessment){
+        if (assessment < 2) throw new IllegalArgumentException(" Минимально допустимая оценка это 2. Измените указанную вами оценку" + assessment);
+        if (assessment > 5) throw new IllegalArgumentException(" Максимально допустимая оценка это 5. Измените указанную вами оценку" + assessment);
+        return true;
     }
 
-
-    public void setAssessments(int [] value) {
-        for (int x: value){
-        if (x < 2 || x > 5){
-            throw new IllegalArgumentException("Вы указали число: "+ x +"\t Число не может быть меньше 2 и выше 5");
-            }
-        else
-            {
-                assessment.add(x);
-        }
-
-
-        }
-
+    public String getName() {
+        return name;
     }
-    public  void changeAssessment(int index, int value){
-        if (index < 0 ) throw new IllegalArgumentException( index +
-                "Указано значение меньше нуля. Значение индекса может быть больше или равно 0");
-        if (value > 1 && value < 5) throw new IllegalArgumentException("Вы указали не соответсвующую оценку: "
-                + value +"\t Оценка не может быть меньше 2 и выше 5");
-       this.assessment.set(index,value);
+
+    public List<Integer> getAssessment() {
+        return assessment;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAssessment(int ... assessment) {
+        for (int i : assessment) {
+            chekAssessment(i);
+            this.assessment.add(i);
+        }
+    }
+    public  void  changeAssessment(int index, int assessment){
+        chekAssessment(assessment);
+        if (index-1 > this.assessment.size()-1 && index < 0) throw new IllegalArgumentException("Не верно указан номер оценки. Укажите значение от 1 до " + this.assessment.size());
+        this.assessment.set(index-1, assessment);
+    }
+    public  void  changeAllAssessment( int ... assessment){
+
+        this.assessment.clear();
+        for (int i : assessment) {
+            chekAssessment(i);
+            this.assessment.add(i);
+        }
+
     }
 
     @Override
     public String toString() {
-        return  fullName.toString() +
-                " [" + getAssessment() +
-                ']';
+
+        return  name + ": " + assessment;
     }
 }
+
+
+/*
+@todo
+Студент: getAssassments возвращает ссылку на внутренний список и через неё можно будет добавить какие хочешь значения.
+метод getAssassment вообще не пойми что такое, и код бы отформатировать перед заливкой на гит. Ну и там ряда методов по заданию не хватает.
+
+1.3.1
+Студент. Создайте сущность Студент, которая описывается:
+•	Имя: строка
+•	Оценки: массив целых чисел.
+•	Может возвращать текстовое представление вида “Имя: [оценка1, оценка2,…,оценкаN]”
+Необходимо выполнить следующие задачи:
+1.	Создать студента Васю с оценками: 3,4,5.
+2.	Создать студента Петю и скопировать оценки Васи, присвоив содержимое поля с оценками Васи полю с оценками Пети.
+3.	Заменить первую оценку Пети на число 5. Вывести на экран строковое представление Васи и Пети. Объяснить результат
+4.	Создать студента Андрея и скопировать ему оценки Васи так, чтобы изменение оценок Васи не влияло на Андрея.
+
+
+
+1.4.7
+Создаем Студента. Измените сущность Студент из задачи 1.3.1. Новые требования включают:
+•	Студента можно создать указав только имя
+•	Студента можно создать указав имя и набор оценок
+•	Оба способа должны быть реализованы единственным конструктором
+Создайте и выведите на экран следующие объекты:
+1.	Студент Вася с оценками: 3,4,5.
+2.	Студент Максим без оценок
+
+
+1.5.6
+Студент отличник. Измените сущность Студент из задачи 1.4.7. Новые требования включают:
+•	Студент может возвращать значение своей средней оценки (вещественное число). Если оценок нет, то среднее – 0.
+•	Студент может возвращать информацию о том, является ли он отличником или нет. Студент является отличником если у него есть хотя бы одна оценка и все оценки только отличные.
+Необходимо выполнить следующие задачи:
+1.	Создайте студента Васю с оценками 3,4,5,4
+2.	Создайте студента Петю с оценками 5,5,5,5
+3.	Выведите для обоих студентов информацию о среднем балле и являются ли они отличниками
+
+
+1.6.9
+Диапазон оценок. Измените сущность Студент из задачи 1.5.6.
+Необходимо гарантировать, что добавлять Студенту можно только оценки в диапазоне от 2 до 5, при этом у Студента всегда можно узнать список оценок.
+Продемонстрируйте на примерах, что нет способа задать Студенту некорректную оценку.
+
+ */
