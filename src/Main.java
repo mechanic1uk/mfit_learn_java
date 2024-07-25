8ede8ebdb4ffed570b15902078077def23edec39
M0nk <ea.chukhontsev@icloud.com>
2024-07-25 11:29:47 +0300

    1.5.7 update

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,24 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-       City a = new City("A");
-       City b = new City("B");
-       City c = new City("C");
-       City d = new City("D");
-       City e = new City("E");
-       Map <City, Integer>  listcity = new HashMap<>();
-       listcity.put(b,100);
-       listcity.put(c,200);
-       listcity.put(d,300);
-       listcity.put(e,400);
-       City f = new City("F", listcity);
-       f.addPath(a,1);
-       System.out.println(f);
+      Polyline pln = new Polyline();
 
-
-       a.addPath(b,11);
-       a.addPath(c,21);
-
-       System.out.println(a);
-       System.out.println(f);
-
-       a.removePath(b);
-       f.removePath(a);
-       f.removePath(d);
-       f.removePath(e);
-       System.out.println(a);
-       System.out.println(f);
-       f.removePath(e);
-
-
-       f.addPath(b,99);
-
-
-
-
-
-
-
-
+      System.out.println(pln);
+      System.out.println(pln.distance());
 
 
     }
 
+
+
+
+
+
 }
+
+
