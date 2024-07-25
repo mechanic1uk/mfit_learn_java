dbf1c86bebffbf9e627aa082f2cc553429b00cef
M0nk <ea.chukhontsev@icloud.com>
2024-07-25 08:49:43 +0300

    1.4.9 update

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
-       Point p1 = new Point(1,5);
-       Point p2 = new Point(2,8);
-       Point p3 = new Point(5,3);
-       Polyline pln1 = new Polyline();
-       pln1.addPoint(p1,p2,p3);
-       Polyline pln2 = new Polyline(pln1.getFirstPoint(), new Point(2,-5), new Point(4,-8), pln1.getLastPoint());
-       System.out.println(pln1);
-       System.out.println(pln2);
-       pln1.getFirstPoint().setXY(11,22);
-       pln1.getLastPoint().setXY(33,32);
-       System.out.println(pln1);
-       System.out.println(pln2);
+      Polyline pln = new Polyline();
+      Polyline pln2 = new Polyline( new Point(1,2), new Point(2,3 ), new Point(3,4));
+      pln.addPoint(new Point(33,2));
+      System.out.println(pln);
+
+      System.out.println(pln2);
 
 
     }
